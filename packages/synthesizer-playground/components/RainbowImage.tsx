492775b4ab9690f93d52c3ac058abe00f6109c0a
SonYoungsung <sysgigigi@gmail.com>
2025-03-06 17:15:33 +0900

    feat: animation proto

--- packages/synthesizer-playground/components/RainbowImage.tsx
+++ packages/synthesizer-playground/components/RainbowImage.tsx
@@ -1,13 +1,51 @@
+import Image from 'next/image';
+import Bani from "../assets/bani.svg"
+
 export default function RainbowImage() {
+ 
   return (
+    <div className='flex w-full'
+      style={{
+        animation: "moveOnce 5s"
+    }}
+    > 
     <div 
       className="w-full h-[50px] z-[1000]" 
       style={{ 
         backgroundImage: "url('/assets/rainbow.svg')", 
         backgroundRepeat: "repeat-x",
         backgroundSize: "auto 100%",
-        backgroundPosition: "center bottom"
+        backgroundPosition: "center bottom",
+        // animation: "moveOnce 10s"
       }}
     />
+      <div className='absolute w-full h-[73px]'
+        style={{
+          // animation: "moveBani 10s"
+        }}
+      >
+        <Image src={Bani} alt="Rainbow"
+          width={113} height={73} className='absolute top-[-10px] right-[-113px]' />
+      </div>
+       <style jsx>{`
+        @keyframes moveOnce {
+          from {
+            transform: translateX(-100%);
+          }
+          to {
+            transform: translateX(0%);
+          }
+          }
+          @keyframes moveBani {
+          0% {
+            transform: translateX(-100%);
+             display: block;
+          }
+          100% {
+            transform: translateX(0%);
+            display: none;
+          }
+      `}</style>
+      </div>
   );
 } 
