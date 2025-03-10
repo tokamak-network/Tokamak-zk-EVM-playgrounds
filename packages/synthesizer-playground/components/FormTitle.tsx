c8ca1a5cf2314690ecf511521a2b7dac8ace3eb3
SonYoungsung <sysgigigi@gmail.com>
2025-03-10 13:10:27 +0900

    fix: header title font size

--- packages/synthesizer-playground/components/FormTitle.tsx
+++ packages/synthesizer-playground/components/FormTitle.tsx
@@ -1,9 +1,11 @@
+import { useViewport } from "@/hooks/useMediaView";
+
 export default function FormTitle({ isResultsShown }: { isResultsShown: boolean }) {
-    
+    const {isOverBreakpoint} = useViewport()
   return (
      <div className={`flex justify-center items-center`}>
         <div 
-          className={`${isResultsShown ? 'text-[42px] leading-[42px]' : 'text-[96px] leading-[96px]'} font-jerse text-[#FAFE00] flex ${isResultsShown ? 'flex-row' : 'flex-col'}`}
+          className={`${isResultsShown ? 'text-[42px] leading-[42px]' : isOverBreakpoint ? 'text-[96px] leading-[96px]' : 'text-[70px] leading-[70px]'} font-jerse text-[#FAFE00] flex ${isResultsShown ? 'flex-row' : 'flex-col'}`}
           style={{ 
             textShadow: '0px 2px 0px rgba(161, 117, 16, 1.00)'
           }}
