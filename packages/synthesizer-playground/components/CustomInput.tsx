9aa770359cc1b172c271affdc03de4c804f03838
SonYoungsung <sysgigigi@gmail.com>
2025-03-10 17:02:15 +0900

    feat: focus animation effect

--- packages/synthesizer-playground/components/CustomInput.tsx
+++ packages/synthesizer-playground/components/CustomInput.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 interface CustomInputProps {
   value?: string;
@@ -23,28 +23,22 @@ const CustomInput = ({
   };
 
   return (
-    <div className="w-[552px] h-[59px] flex-col justify-center items-start inline-flex">
-      <div className="self-stretch h-px bg-[#a8a8a8]" />
-      <div className="self-stretch h-14 bg-[#f7f7f7] justify-center items-center gap-2 inline-flex">
-        <div className="w-px self-stretch bg-[#a8a8a8]" />
-        <div className="grow shrink basis-0 h-[33px] pb-0.5 justify-start items-start flex">
-          <input
-            type="text"
-            className={`w-full h-full border-none bg-transparent px-4 font-ibm-mono text-2xl font-normal leading-[31.2px] outline-none placeholder:text-[#999999] focus:placeholder:opacity-0 ${
-              error ? 'text-[#a01313]' : active || value ? 'text-[#222222]' : 'text-[#999999]'
-            }`}
-            value={value}
-            onChange={(e) => onChange(e.target.value)}
-            placeholder="Enter transaction ID"
-            disabled={disabled}
-            onFocus={() => setActive(true)}
-            onBlur={handleBlur}
-          />
-        </div>
-        <div className="w-px self-stretch bg-[#5f5f5f]" />
-      </div>
-      <div className="self-stretch h-px bg-[#5f5f5f]" />
-      <div className="self-stretch h-px" />
+    <div className='relative'>
+      <input
+        type="text"
+        className={`w-[552px] h-[59px] border-none px-4 font-ibm-mono text-2xl font-normal leading-[31.2px] placeholder:text-[#999999] outline-none
+          ${error ? 'text-[#a01313]' : active || value ? 'text-[#222222]' : 'text-[#999999]'}`}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        placeholder="Enter transaction ID"
+        disabled={disabled}
+        onFocus={() => setActive(true)}
+        onBlur={handleBlur}
+      />
+     {/* 배경용 div */}
+      <div className={`absolute inset-0 bg-[#5B9AFF] opacity-0 pointer-events-none ${active ? 'animate-focusEffect' : ''}`} />
+      {/* 테두리용 div */}
+      <div className={`absolute inset-0 border-2 border-[#5B9AFF] opacity-0 pointer-events-none ${active ? 'animate-focusEffect' : ''}`} />
     </div>
   );
 };
