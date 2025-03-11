deba9b20620bc2530f05547cc57a174791b68283
SonYoungsung <sysgigigi@gmail.com>
2025-03-11 18:24:05 +0900

    fix: input style under braking point

--- packages/synthesizer-playground/components/FormTitle.tsx
+++ packages/synthesizer-playground/components/FormTitle.tsx
@@ -1,11 +1,11 @@
 import { useViewport } from "@/hooks/useMediaView";
 
-export default function FormTitle({ isResultsShown, isProcessing }: { isResultsShown: boolean, isProcessing: boolean }) {
+export default function FormTitle({ isResultsShown, isProcessing , needToExpand}: { isResultsShown: boolean, isProcessing: boolean, needToExpand: boolean }) {
   const { isOverBreakpoint } = useViewport();
   return (
      <div className={`flex justify-center items-center`}>
         <div 
-          className={`${isResultsShown || (!isOverBreakpoint && isProcessing) ? 'text-[42px] leading-[42px]' : isOverBreakpoint ? 'text-[96px] leading-[96px]' : 'text-[70px] leading-[70px]'} font-jerse text-[#FAFE00] flex ${isResultsShown || (!isOverBreakpoint && isProcessing) ? 'flex-row' : 'flex-col'}`}
+          className={`${isResultsShown || (!isOverBreakpoint && isProcessing || needToExpand) ? 'text-[42px] leading-[42px]' : isOverBreakpoint ? 'text-[96px] leading-[96px]' : 'text-[70px] leading-[70px]'} font-jerse text-[#FAFE00] flex ${isResultsShown || (!isOverBreakpoint && isProcessing || needToExpand) ? 'flex-row' : 'flex-col'}`}
           style={{ 
             textShadow: '0px 2px 0px rgba(161, 117, 16, 1.00)'
           }}
