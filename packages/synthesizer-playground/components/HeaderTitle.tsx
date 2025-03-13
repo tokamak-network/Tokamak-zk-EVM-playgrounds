76b74390f08c2836421c0db2a7e26819fab02918
SonYoungsung <sysgigigi@gmail.com>
2025-03-13 14:34:16 +0900

    fix: result display responsive height

--- packages/synthesizer-playground/components/HeaderTitle.tsx
+++ packages/synthesizer-playground/components/HeaderTitle.tsx
@@ -1,5 +1,4 @@
 export default function HeaderTitle({ children, shouldShowResults, isSmallScreen }: { children: React.ReactNode, shouldShowResults: boolean, isSmallScreen: boolean }) {
-
     
     return (
     <div className={`flex flex-col ${shouldShowResults || isSmallScreen ? 'gap-y-[17px]' : 'gap-y-[32px]'}`}>
