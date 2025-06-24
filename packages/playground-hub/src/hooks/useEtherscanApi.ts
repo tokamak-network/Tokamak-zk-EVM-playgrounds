ecc55e7cc9d98e1bf7e59af71f5a5ca8d95bdaca
SonYoungsung <sysgigigi@gmail.com>
2025-06-24 18:17:15 +0900

    feat: remove a process to check valid etherscan api key

--- packages/playground-hub/src/hooks/useEtherscanApi.ts
+++ packages/playground-hub/src/hooks/useEtherscanApi.ts
@@ -2,28 +2,28 @@ import { useState, useEffect, useRef } from "react";
 import { validateEtherscanApiKey } from "../utils/checkEtherscanApi";
 
 export const useDebouncedEtherscanValidation = (etherscanApiKey: string) => {
-  const [isValid, setIsValid] = useState<boolean | null>(null);
+  const [isValid, setIsValid] = useState<boolean | null>(true);
   const debounceRef = useRef<NodeJS.Timeout | null>(null);
 
-  useEffect(() => {
-    // 입력이 없으면 검증하지 않음
-    if (!etherscanApiKey) {
-      setIsValid(null);
-      return;
-    }
+  // useEffect(() => {
+  //   // 입력이 없으면 검증하지 않음
+  //   if (!etherscanApiKey) {
+  //     setIsValid(null);
+  //     return;
+  //   }
 
-    // debounce
-    if (debounceRef.current) clearTimeout(debounceRef.current);
-    debounceRef.current = setTimeout(async () => {
-      const valid = await validateEtherscanApiKey(etherscanApiKey);
-      setIsValid(valid);
-    }, 500);
+  //   // debounce
+  //   if (debounceRef.current) clearTimeout(debounceRef.current);
+  //   debounceRef.current = setTimeout(async () => {
+  //     const valid = await validateEtherscanApiKey(etherscanApiKey);
+  //     setIsValid(valid);
+  //   }, 500);
 
-    // cleanup
-    return () => {
-      if (debounceRef.current) clearTimeout(debounceRef.current);
-    };
-  }, [etherscanApiKey]);
+  //   // cleanup
+  //   return () => {
+  //     if (debounceRef.current) clearTimeout(debounceRef.current);
+  //   };
+  // }, [etherscanApiKey]);
 
   return { isValid };
 };
